6424d0f4be9f7d220753b7c3e51828678d872e6d
Lee hwee <145762399+hwee2@users.noreply.github.com>
2024-01-03 16:34:18 +0900

    Update printItem.py

--- printItem.py
+++ printItem.py
@@ -1,7 +1,7 @@
 def printItem() :
-    "변수명을 정해주세요" = [1,2,3,4,5,6,7,8,9]  # 변수명 수정필요
+    List = [1,2,3,4,5,6,7,8,9]  # 변수명 수정필요
 
-    for n in li:
+    for n in List:
         print(n)
 
 printItem()
